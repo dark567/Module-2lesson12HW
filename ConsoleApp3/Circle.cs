64573def279c0456d80e07f8ad3e0ade6c161fed
dark567 <darkmit@list.ru>
2019-09-11 20:49:54 +0300

    201909112049

---
+++ ConsoleApp3/Circle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class Circle : Figure
+    {
+        public override double Perimeter()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double Squire()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ConsoleApp3/Figure.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public abstract class Figure
+    {
+        public abstract double Squire();
+        public abstract double Perimeter();
+    }
+}
